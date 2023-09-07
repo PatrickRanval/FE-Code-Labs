d646e7dee2fa4844dc207ee1aa56ee1552bb9bb4
PatrickRanval <pat.ranval@gmail.com>
2023-09-07 12:35:53 -0500

    Getting Folder Structure Updated
    Updating Folder Structure for increased organization

---
+++ Class-5/exercise1/js/main.js
@@ -0,0 +1,20 @@
+function makeCounter() {
+  let count = 0;
+  
+  return function() {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+alert( counter() ); 
+alert( counter() ); 
+alert( counter() ); 
+alert( makeCounter() );
+
+
+
+
+
+
+
